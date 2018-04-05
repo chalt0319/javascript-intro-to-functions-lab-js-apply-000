e175b32649eb30c470af2461237384780c7fe718
chalt0319@gmail.com <chalt0319@gmail.com>
2018-04-05 23:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCaserCase())
   return "I can't hear you!"
-  else if (string.toUpperCase())
+  else if (string === string.toUpperCase())
   return "YES INDEED!"
 }
